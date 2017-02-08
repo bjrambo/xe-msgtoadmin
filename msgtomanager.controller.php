b0fc85ea302587b8b03ee57fa278de27a3cedabb
BJRambo <qw5414@naver.com>
2017-02-08 19:45:47 +0900

    Improved sendmessage method argument.

--- msgtomanager.controller.php
+++ msgtomanager.controller.php
@@ -11,38 +11,36 @@ class msgtomanagerController extends msgtomanager
 	function procMsgtomanagerSendMessage()
 	{
 		$logged_info = Context::get('logged_info');
+		$obj = Context::getRequestVars();
 
-		// Check variables
-		$receiver_srl = Context::get('receiver_srl');
-		if(!$receiver_srl)
+		if(!$obj->email)
 		{
-			return new Object(-1, 'msg_not_exists_member');
+			return new Object(-1, '이메일 값은 필수 입력항목입니다.');
 		}
 
-		$title = trim(Context::get('title'));
-		if(!$title)
+		$obj->title = trim($obj->title);
+		if(!$obj->title)
 		{
 			return new Object(-1, 'msg_title_is_null');
 		}
 
-		$content = trim(Context::get('content'));
-		if(!$content)
+		$obj->content = trim($obj->content);
+		if(!$obj->content)
 		{
 			return new Object(-1, 'msg_content_is_null');
 		}
 
-		$send_mail = Context::get('send_mail');
-		if($send_mail != 'Y')
+		if($obj->send_mail != 'Y')
 		{
-			$send_mail = 'N';
+			$obj->send_mail = 'N';
 		}
 
 		// Check if there is a member to receive a message
 		$oMemberModel = getModel('member');
 		$oCommunicationModel = getModel('communication');
 
-		$receiver_member_info = $oMemberModel->getMemberInfoByMemberSrl($receiver_srl);
-		if($receiver_member_info->member_srl != $receiver_srl)
+		$receiver_member_info = $oMemberModel->getMemberInfoByMemberSrl($obj->receiver_srl);
+		if($receiver_member_info->member_srl != $obj->receiver_srl)
 		{
 			return new Object(-1, 'msg_not_exists_member');
 		}
@@ -64,7 +62,7 @@ class msgtomanagerController extends msgtomanager
 		}
 
 		// send a message
-		$output = $this->sendMessage($logged_info->member_srl, $receiver_srl, $title, $content);
+		$output = $this->sendMessage($logged_info->member_srl, $obj);
 
 		if(!$output->toBool())
 		{
@@ -72,12 +70,12 @@ class msgtomanagerController extends msgtomanager
 		}
 
 		// send an e-mail
-		if($send_mail == 'Y')
+		if($obj->send_mail == 'Y')
 		{
 			$view_url = Context::getRequestUri();
-			$content = sprintf("%s<br /><br />From : <a href=\"%s\" target=\"_blank\">%s</a>", $content, $view_url, $view_url);
+			$content = sprintf("%s<br /><br />From : <a href=\"%s\" target=\"_blank\">%s</a>", $obj->content, $view_url, $view_url);
 			$oMail = new Mail();
-			$oMail->setTitle(htmlspecialchars($title, ENT_COMPAT | ENT_HTML401, 'UTF-8', false));
+			$oMail->setTitle(htmlspecialchars($obj->title, ENT_COMPAT | ENT_HTML401, 'UTF-8', false));
 			$oMail->setContent(utf8_mbencode(removeHackTag($content)));
 			$oMail->setSender($logged_info->nick_name, $logged_info->email_address);
 			$oMail->setReceiptor($receiver_member_info->nick_name, $receiver_member_info->email_address);
@@ -91,81 +89,88 @@ class msgtomanagerController extends msgtomanager
 			$this->setRedirectUrl($returnUrl);
 		}
 
-		return $output;
+		//return $output;
+
 	}
 
-	function sendMessage($sender_srl, $receiver_srl, $title, $content, $sender_log = TRUE)
+	/**
+	 * @param int $sender_srl
+	 * @param object $obj
+	 * @param bool $sender_log
+	 * @return object
+	 */
+	function sendMessage($sender_srl, $obj, $sender_log = true)
 	{
-		$logged_info = Context::get('logged_info');
-
 		// Encode the title and content.
-		$title = htmlspecialchars($title, ENT_COMPAT | ENT_HTML401, 'UTF-8', false);
-		$content = removeHackTag($content);
+		$title = htmlspecialchars($obj->title, ENT_COMPAT | ENT_HTML401, 'UTF-8', false);
+		$content = removeHackTag($obj->content);
 		$title = utf8_mbencode($title);
 		$content = utf8_mbencode($content);
 
 		$message_srl = getNextSequence();
 		$related_srl = getNextSequence();
 
-		// messages to save in the sendor's message box
-		$sender_args = new stdClass();
-		$sender_args->sender_srl = $sender_srl;
-		$sender_args->receiver_srl = $receiver_srl;
-		$sender_args->message_type = 'S';
-		$sender_args->title = $title;
-		$sender_args->content = $content;
-		$sender_args->readed = 'N';
-		$sender_args->regdate = date("YmdHis");
-		$sender_args->message_srl = $message_srl;
-		$sender_args->related_srl = $related_srl;
-		$sender_args->list_order = $sender_args->message_srl * -1;
+		$admin_args = new stdClass();
+		$admin_args->is_admin = 'Y';
+		$output = executeQueryArray('member.getMemberList', $admin_args);
+		$adminMembers = $output->data;
 
-
-		// Call a trigger (before)
-		$trigger_obj = new stdClass();
-		$trigger_obj->sender_srl = $sender_srl;
-		$trigger_obj->receiver_srl = $receiver_srl;
-		$trigger_obj->message_srl = $message_srl;
-		$trigger_obj->related_srl = $related_srl;
-		$trigger_obj->title = $title;
-		$trigger_obj->content = $content;
-		$trigger_obj->sender_log = $sender_log;
-		$trigger_obj->nick_name = $logged_info->nick_name;
-		$trigger_obj->email = $logged_info->email_address;
-
-		$trigger_output = ModuleHandler::triggerCall('communication.sendMessage', 'before', $trigger_obj);
-		if(!$trigger_output->toBool())
+		if(count($adminMembers) > 0)
 		{
-			return $trigger_output;
-		}
+			$oDB = DB::getInstance();
+			$oDB->begin();
 
-		$isSend = $this->sendLogInsert($trigger_obj);
-		if(!$isSend->toBool())
-		{
-			return $isSend;
-		}
-
-		$oDB = DB::getInstance();
-		$oDB->begin();
-
-		// messages to save in the sendor's message box
-		if($sender_srl && $sender_log)
-		{
-			$output = executeQuery('communication.sendMessage', $sender_args);
-			if(!$output->toBool())
+			foreach($adminMembers as $admin)
 			{
-				$oDB->rollback();
-				return $output;
+				// messages to save in the sendor's message box
+				$sender_args = new stdClass();
+				$sender_args->sender_srl = $sender_srl;
+				$sender_args->receiver_srl = $admin->member_srl;
+				$sender_args->message_type = 'S';
+				$sender_args->title = $title;
+				$sender_args->content = $content;
+				$sender_args->readed = 'N';
+				$sender_args->regdate = date("YmdHis");
+				$sender_args->message_srl = $message_srl;
+				$sender_args->related_srl = $related_srl;
+				$sender_args->list_order = $sender_args->message_srl * -1;
+				$sender_args->sender_log = $sender_log;
+				$sender_args->nick_name = 'nick';
+				$sender_args->email = 'email';
+
+				$trigger_output = ModuleHandler::triggerCall('msgtomanager.sendMessage', 'before', $sender_args);
+				if(!$trigger_output->toBool())
+				{
+					return $trigger_output;
+				}
+
+				$isSend = $this->sendLogInsert($sender_args);
+				if(!$isSend->toBool())
+				{
+					return $isSend;
+				}
+
+				// messages to save in the sendor's message box
+				if($sender_srl && $sender_log)
+				{
+					$output = executeQuery('communication.sendMessage', $sender_args);
+					if(!$output->toBool())
+					{
+						$oDB->rollback();
+						return $output;
+					}
+				}
+
+				// Call a trigger (after)
+				ModuleHandler::triggerCall('msgtomanager.sendMessage', 'after', $sender_args);
+
+
+
+				// create a flag that message is sent (in file format)
+				$this->updateFlagFile($admin->member_srl);
 			}
+			$oDB->commit();
 		}
-
-		// Call a trigger (after)
-		ModuleHandler::triggerCall('communication.sendMessage', 'after', $trigger_obj);
-
-		$oDB->commit();
-
-		// create a flag that message is sent (in file format)
-		$this->updateFlagFile($receiver_srl);
 
 		return new Object(0, 'success_sended');
 	}
